37c0ecb717e2a1a19c0d101ad5396d244d239f7c
evcorradi <122548018+evcorradi@users.noreply.github.com>
2023-01-20 11:06:47 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,39 @@
 input.onButtonPressed(Button.A, function () {
-    fair_game = randint(1, 10)
+    basic.showNumber(1,10)
+basic.showNumber(9,10)
+basic.showNumber(10,10)
+basic.showNumber(2,10)
+basic.showNumber(3,10)
+basic.showNumber(4,15)
+basic.showNumber(5,16)
+basic.showNumber(6,17)
+basic.showNumber(7,19)
+basic.showNumber(8,20)
+fair_game = randint(1, 10)
     if (fair_game == 1) {
-    	
-    } else if (fair_game == 1) {
-    	
-    } else if (fair_game == 1) {
-    	
-    } else if (fair_game == 1) {
-    	
-    } else if (fair_game == 1) {
-    	
-    } else if (fair_game == 1) {
-    	
-    } else if (fair_game == 1) {
-    	
-    } else if (fair_game == 1) {
-    	
-    } else if (fair_game == 1) {
-    	
+        basic.showNumber(1)
+    } else if (fair_game == 2) {
+        basic.showNumber(2)
+    } else if (fair_game == 3) {
+        basic.showNumber(3)
+    } else if (fair_game == 4) {
+        basic.showNumber(4)
+    } else if (fair_game == 5) {
+        basic.showNumber(5)
+    } else if (fair_game == 6) {
+        basic.showNumber(6)
+    } else if (fair_game == 7) {
+        basic.showNumber(7)
+    } else if (fair_game == 8) {
+        basic.showNumber(8)
+    } else if (fair_game == 9) {
+        basic.showNumber(9)
     } else {
-    	
+        basic.showNumber(10)
     }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.East)
 })
 input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index < 80; index++) {
@@ -58,6 +71,10 @@ input.onGesture(Gesture.Shake, function () {
         `)
     basic.clearScreen()
 })
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showString("yay you know directions now :)")
+    basic.clearScreen()
+})
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     basic.showLeds(`
         # . . . .
@@ -82,6 +99,8 @@ input.onLogoEvent(TouchButtonEvent.Touched, function () {
         `)
     basic.showIcon(IconNames.Heart)
     basic.clearScreen()
+    basic.showString("tilt left")
 })
 let fair_game = 0
 basic.showString("Hello!")
+basic.clearScreen()
